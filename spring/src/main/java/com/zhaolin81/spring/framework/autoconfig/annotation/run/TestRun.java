6841586186e510550deacbb5744cc2a214f38b2d
zhaolin <zhaolin@ekeyfund.com>
2018-04-26 19:34:52 +0800

    add linkerd+namerd config

---
+++ spring/src/main/java/com/zhaolin81/spring/framework/autoconfig/annotation/run/TestRun.java
@@ -0,0 +1,14 @@
+package com.zhaolin81.spring.framework.autoconfig.annotation.run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zhaolin on 4/19/2018.
+ */
+@SpringBootApplication
+public class TestRun {
+    public static void main(String[] args) {
+        SpringApplication.run(TestRun.class,args);
+    }
+}
